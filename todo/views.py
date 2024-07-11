03584ea1513911ef2ae3c43b5f3b63691fae9a45
s1F102302528-bob <136312529+s1F102302528-bob@users.noreply.github.com>
2024-07-11 12:15:11 +0900

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -36,13 +36,15 @@ def detail(request, task_id):
 
 
 def delete(request, task_id):
+    task.delete()
+    return redirect(index)
+
+
 def update(request, task_id):
-    try:
+     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    task.delete()
-    return redirect(index)
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
